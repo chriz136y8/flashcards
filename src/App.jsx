15b198e6d07f2a50a454d34ed7d64b1f7e365026
chriz136y8 <chrisamoak@gmail.com>
2025-03-07 21:17:10 -0500

    flashcards begin

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Who's that Pokemon?</h1>
+      <h3>Are you the pokemon expert? Test your knowledge?</h3>
+      
+      <div className='header'>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
